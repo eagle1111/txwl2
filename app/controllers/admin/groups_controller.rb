eeac9e375bebb7f1c5b2ddea0ab1cc9865ff567f
eagle1111 <wang499817054@gmail.com>
2017-06-01 11:05:52 +0800

    分组

---
+++ app/controllers/admin/groups_controller.rb
@@ -0,0 +1,40 @@
+class Admin::GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+  layout "admin"
+  def index
+    @groups = Group.all
+  end
+  def new
+    @group = Group.new
+  end
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to admin_groups_path
+    else
+      render :new
+    end
+  end
+  def edit
+    @group = Group.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to admin_groups_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to admin_groups_path
+  end
+
+end
+private
+def group_params
+  params.require(:group).permit(:title, :description)
+end
